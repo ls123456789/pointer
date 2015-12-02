e8800886ae42e1f7096d7ab34e1cf8e165cc96cc
ls123456789 <2953757467@qq.com>
2015-12-02 18:10:22 +0800

    Initial Commit

---
+++ pointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by admin on 15/12/2.
+//  Copyright © 2015年 内师大. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
