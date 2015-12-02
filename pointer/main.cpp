f7112652abeb6835472e6f85f8c40f433e35a466
ls123456789 <2953757467@qq.com>
2015-12-02 20:57:51 +0800

    pointer
    先建立一个int型的数组，和一个int型的指针，给数组赋值，再把数组a的首地址，使p指向a。
    输出若干个值，来表示其中的规律：（1）“&”表示取地址，&a[i]表示取数组i＋1个元素的地址，&p表示变量存储的位置。（2）数组名表示数组的首
    地址，所以输出a的值和&a[0]的值相同。（3）p指针的存储空间中，存的是它所指向的数组空间的首地址，即 a。（4）“＊”
    是间接取值符，表示获取指针变量所指向的地址中存储的数据，＊p表示p指向的地址a[0]中的数据。因为a代表数组的首地址，所以＊a和＊p相同。（5）
    可以用p[i]直接表示a[i],(a+1)表示a的首地址再加一，指向第二个数据a[1]，指针p也可以用相同的办法指向除第一个以外的数据。

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -1,15 +1,26 @@
 //
 //  main.cpp
-//  pointer
+//  test
 //
-//  Created by admin on 15/12/2.
-//  Copyright © 2015年 内师大. All rights reserved.
+//  Created by 梁爽 on 15/11/11.
+//  Copyright (c) 2015年 梁爽. All rights reserved.
 //
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[4];
+    int *p;
+    a[0] = 10;
+    a[1] = 20;
+    a[2] = 30;
+    a[3] = 40;
+    p = a;
+    printf("&a[0] = %x &a[1] = %x &a[2] = %x &a[3] = %x a = %x\n",&a[0],&a[1],&a[2],&a[3],a);
+    printf("p = %x  *p = %d  *a = %d &p = %x\n",p,*p,*a,&p);
+    printf("p[0] = %d  p[1] = %d\n",p[0],p[1]);
+    printf("a[1] = %d\n",*(a+1));
+    printf("p[1] = %d\n",*(p+1));
     return 0;
 }
